6323eff9efeb25225815bda75c731b82983935d9
Alan_Hu <hu_hanwen@163.com>
2025-03-04 14:42:51 +0800

    项目使用element-plus UI框架

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <h2>App组件</h2>
+  <el-button type="danger" :icon="Delete"></el-button>
 </template>
 
-<script lang="ts" setup name="App"></script>
+<script lang="ts" setup name="App">
+import { Delete } from '@element-plus/icons-vue'
+</script>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 
+app.use(ElementPlus, {
+  // 国际化
+  locale: zhCn,
+})
 app.mount('#app')
-
-var a = 100
