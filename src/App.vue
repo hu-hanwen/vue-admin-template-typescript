bc79d47129664c7df3a705e2a0e6499abc6f0310
Alan_Hu <hu_hanwen@163.com>
2025-03-04 15:02:18 +0800

    封装svg通用组件

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <h2>App组件</h2>
   <el-button type="danger" :icon="Delete"></el-button>
+  <SvgIcon name="full-screen"></SvgIcon>
 </template>
 
 <script lang="ts" setup name="App">
 import { Delete } from '@element-plus/icons-vue'
+import SvgIcon from '@/components/SvgIcon/index.vue'
 </script>
 
 <style scoped></style>
---
+++ src/components/SvgIcon/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <svg :style="{ width, height }">
+    <use :xlink:href="`#${prefix}-${name}`" :fill="color"></use>
+  </svg>
+</template>
+
+<script lang="ts" setup name="SvgIcon">
+defineProps({
+  width: {
+    type: String,
+    default: '16px',
+  },
+  height: {
+    type: String,
+    default: '16px',
+  },
+  color: {
+    type: String,
+    default: '#333',
+  },
+  prefix: {
+    type: String,
+    default: 'icon',
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<style scoped></style>
